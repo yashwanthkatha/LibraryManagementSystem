e5f7d233b3a0b213b11d1f977e61016c61132db8
anandatheerthank <31877664+anandatheerthank@users.noreply.github.com>
2025-03-26 10:59:27 +0530

    Update Program.cs

--- LibraryManagementSystem/Program.cs
+++ LibraryManagementSystem/Program.cs
@@ -1,10 +1,10 @@
-using LibraryManagementSystem.Data;
-using LibraryManagementSystem.Interfaces;
-using LibraryManagementSystem.Repositories;
 using Microsoft.EntityFrameworkCore;
-
+using RetailManagementSystem.Data;
+using RetailManagementSystem.Interfaces;
+using RetailManagementSystem.Repositories;
+ 
 var builder = WebApplication.CreateBuilder(args);
-
+ 
 // Add services to the container.
 builder.Services.AddDbContext<LibraryContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection")));
@@ -12,25 +12,35 @@ builder.Services.AddDbContext<LibraryContext>(options =>
 builder.Services.AddScoped<IBook,BookRepository>();
 builder.Services.AddScoped<IMember, MemberRepository>();
 builder.Services.AddScoped<IBorrowRecord, BorrowRecordRepository>();
-
+ 
 builder.Services.AddControllers();
+ 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+ 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+ 
+// Enable Swagger in both Development and Production
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+});
+ 
+// Redirect root URL to Swagger UI
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/")
+    {
+        context.Response.Redirect("/swagger/index.html");
+        return;
+    }
+    await next();
+});
+ 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
+ 
 app.Run();
